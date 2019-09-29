aa304ccdcb83989e452fd6ccefd4a054c2db6c96
customaddone <custom1485@gmail.com>
2019-09-29 10:10:17 +0000

    Operation check

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,10 +1,16 @@
 <html>
 <head>
   <title>RailsTutorial</title>
+  <!-- Vueのインストールを行う -->
+  <script src="https://unpkg.com/vue@2.5.17"></script>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <!-- テンプレート用 -->
 <body>
+  <div id="app">
+      <!-- `greet` は、あらかじめ定義したメソッドの名前 -->
+      <button v-on:click="greet">Greet</button>
+  </div>
   <!-- ４　npm run devでソースをコンパイルする
        ５  view内でasset('/js/()')でassetを読み込む -->
   <script src="{{ asset('/js/samplevue.js') }}"></script>
