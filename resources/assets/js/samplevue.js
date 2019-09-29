f2e7ae726bf17e2178660fe7dd3d8e88d1083c53
customaddone <custom1485@gmail.com>
2019-09-29 10:52:16 +0000

    edit data

--- resources/assets/js/samplevue.js
+++ resources/assets/js/samplevue.js
@@ -1,23 +1,53 @@
 new Vue({
   el: '#app',
   data: {
-    // newItemに一時収納、todosに恒久的に収納
-    newItem: '',
-    todos: []
+    text: "", // 編集覧の値
+
+    editIndex: -1, // 現在編集している要素のインデックス
+                   // 編集状態でない場合は-1を設定
+
+    items: [       // 要素のリストをデフォルトで3つ設定
+        "タスク１",
+        "タスク２",
+        "タスク３"
+    ]
+  },
+  computed: {
+    changeButtonText() {
+        return this.editIndex === -1 ? "追加" : "編集";
+    }
   },
   methods: {
-    addItem: function(){
-      // this.todosで場所指定、pushでtodosに入れる
-      this.todos.push(this.newItem);
-      // newItemは空にする
-      this.newItem = '';
+
+    // 送信ボタンをクリックしたら以下を実行
+    setItems() {
+        if(this.editIndex === -1) {
+            // 配列に要素を追加 一番後ろに追加
+            this.items.push(this.text);
+        } else {
+            // 編集（指定位置から指定した要素数を削除し新しい要素に入れ替え）
+            // this.itemsのthis.Index番目から1つ目をthis.textに変更する
+            this.items.splice(this.editIndex, 1, this.text);
+        }
+        this.cancel();
     },
+
+    // クリア
+    cancel() {
+        this.text = ""; //編集覧の状態をクリア
+        this.editIndex = -1;
+    },
+
+    // 編集（指定した要素を編集状態に設定）
+    edit(index) {
+        this.editIndex = index;
+        this.text = this.items[index];
+        this.$refs.editor.focus(); // フォーカスを設定
+    },
+
     // 削除
-    deleteItem: function(todo) {
-      // todos配列の todo から key を取得
-      var index = this.todos.indexOf(todo);
-      // key番目から１つ削除
-      this.todos.splice(index, 1);
+    remove(index) {
+        this.items.splice(index, 1);
     }
   }
 })
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -8,25 +8,38 @@
 <!-- テンプレート用 -->
 <body>
   <div id="app">
+      <!-- 入力フォーム -->
       <form v-on:submit.prevent="addItem" class="form-group">
           <div class="form-group">
               <div class="input-group">
-                  <input type="text" v-model="newItem" class="form-control">
-                  <span class="input-group-btn"><button class="btn btn-primary" type="submit">送信</button></span>
+                  <!-- v-modelでテキストの値を操作 -->
+                  <input type="text" v-model="text" v-on:keyup.enter="changeItems" ref="editor" class="form-control">
+                  <!-- setItemで追加 {{changeButtonText}}は入力の内容により変わる -->
+                  <span class="input-group-btn"><button v-on:click="setItems" class="btn btn-primary" type="submit">@{{changeButtonText}}</button></span>
               </div>
           </div>
       </form>
+
+      <hr>
+
       <!--ループ処理-->
       <ul class="list-group">
-          <li class="list-group-item" v-for="todo in todos">@{{ todo }}
+          <!-- キーで指定したインデックスがVueに送られ、editIndexの値になる -->
+          <li class="list-group-item" v-for="(item, index) in items" v-bind:key="index">
+              @{{ item }}
               <span class="pull-right">
-                  <button class="btn btn-xs btn-warning" v-on:click="deleteItem(todo)">
-                      削除
-                  </button>
-              </span>
-          </li>
-      </ul>
-  </div>
+                  <!-- edit,remove両方でインデックスを渡す -->
+                  <button class="btn btn-xs btn-info" v-on:click="edit(index)">
+                　     編集
+            　    </button>
+
+            　    <button class="btn btn-xs btn-warning" v-on:click="remove(index)">
+            　         削除
+            　    </button>
+            　</span>
+        　　</li>
+        </ul>
+    </div>
   <!-- ４　npm run devでソースをコンパイルする
        ５  view内でasset('/js/()')でassetを読み込む -->
   <script src="{{ asset('/js/samplevue.js') }}"></script>
