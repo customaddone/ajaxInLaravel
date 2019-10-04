bdef675c611d0d417e0b085cd66c447074e80c4d
customaddone <custom1485@gmail.com>
2019-10-04 12:35:45 +0000

    javaScript Editor

--- resources/views/editors/home.blade.php
+++ resources/views/editors/home.blade.php
@@ -1,25 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Title</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/codemirror.css') }}">
-    <script src="{{ asset('/js/codemirror.js') }}"></script>
-    <script src="{{ asset('/js/javascript.js') }}"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-</head>
-<body>
-    <textarea id="editor_js" rows="30" cols="50"></textarea>
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
 
-    <script type="text/javascript">
-        var editor = CodeMirror.fromTextArea(document.getElementById('editor_js'), {
-            mode: "javascript",
-            lineNumbers: true,
-            indentUnit: 4
-        });
-        editor.save();
-        var text = $("#editor_js").val();
-    </script>
-</body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atelier-dune-dark.min.css">
+  </head>
+  <body>
+    <textarea id="mdraw" style="width:49vw; height:100vh;"># Write Something Here!
+
+```
+function $initHighlight(block, cls) {
+  try {
+    if (cls.search(/\bno\-highlight\b/) != -1)
+      return process(block, true, 0x0F) +
+         ' class=""';
+  } catch (e) {
+  /* handle exception */
+  }
+  for (var i = 0 / 2; i < classes.length; i++) {
+    if (checkCondition(classes[i]) === undefined)
+      return /\d+[\s/]/g;
+  }
+}
+```</textarea>
+    <div id="mdrender" style="width:49vw; height:100vh; float: right;"></div>
+  </body>
+  <script>
+    document.getElementById("mdrender").innerHTML = marked(document.getElementById("mdraw").innerHTML);
+    hljs.initHighlightingOnLoad();
+    $(function() {
+      $("#mdraw").on('keyup', function() {
+        var string = $("#mdraw").val()
+        $("#mdrender").html(marked(string));
+      })
+    })
+  </script>
 </html>
